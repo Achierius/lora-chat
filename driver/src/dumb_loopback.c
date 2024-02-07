b831f4fa58d8d8f29d744637617e6c8553eaed5b
Marcus Plutowski <achierius@gmail.com>
2024-02-07 12:57:34 -0800

    driver: separate ioctl definitions into include directory

--- driver/src/dumb_loopback.c
+++ driver/src/dumb_loopback.c
@@ -8,6 +8,8 @@
 #include <linux/ioctl.h>
 #include <linux/spi/spi.h>
 
+#include "lora-chat/ioctls.h"
+
 #define DEVICE_NAME "dumb-loopback-0"
 #define CLASS_NAME "dumb-loopback"
 #define BUFFER_SIZE 1024
@@ -74,24 +76,6 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
   return bytes_written;
 }
 
-#define IOCTL_MAGIC 0xA8
-// Network characteristics
-#define IOCTL_SET_FREQUENCY        _IOW(IOCTL_MAGIC,  1, int32_t*)
-#define IOCTL_GET_FREQUENCY        _IOR(IOCTL_MAGIC,  2, int32_t*)
-#define IOCTL_SET_SENDER_ADDRESS   _IOW(IOCTL_MAGIC,  3, char*)
-#define IOCTL_GET_SENDER_ADDRESS   _IOR(IOCTL_MAGIC,  4, char*)
-#define IOCTL_SET_RECEIVER_ADDRESS _IOW(IOCTL_MAGIC,  5, char*)
-#define IOCTL_GET_RECEIVER_ADDRESS _IOR(IOCTL_MAGIC,  6, char*)
-// Special commands
-#define IOCTL_SEND_GPS_LOCATION    _IO(IOCTL_MAGIC,   8)
-// Driver internal stuff
-// Transmission characteristics
-#define IOCTL_SET_BANDWIDTH        _IOW(IOCTL_MAGIC, 10, size_t*)
-#define IOCTL_GET_BANDWIDTH        _IOR(IOCTL_MAGIC, 11, size_t*)
-#define IOCTL_SET_CODING_RATE      _IOW(IOCTL_MAGIC, 12, size_t*)
-#define IOCTL_GET_CODING_RATE      _IOR(IOCTL_MAGIC, 13, size_t*)
-#define IOCTL_SET_SPREADING_FACTOR _IOW(IOCTL_MAGIC, 14, size_t*)
-#define IOCTL_GET_SPREADING_FACTOR _IOR(IOCTL_MAGIC, 15, size_t*)
 
 #define IOCTL_SEND_GPS _IO
 static long dev_ioctl(struct file *filep, unsigned int cmd, unsigned long arg) {
