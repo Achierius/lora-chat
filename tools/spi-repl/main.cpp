2b7d67f1c862b09e4b6f90828eb03d8fbc4a4d08
Marcus Plutowski <achierius@gmail.com>
2025-02-09 15:15:34 -0800

    spi-repl: tear out old transmit/recv code
    It's very out of date and has been well-and-above supplanted by the more
    purpose-built lora-chat minitool.

--- tools/spi-repl/main.cpp
+++ tools/spi-repl/main.cpp
@@ -67,59 +67,6 @@ void burst_read_from_spi_cmd(int fd, uint8_t addr, int len) {
   }
 }
 
-void hardcoded_init_for_transmit_cmd(int fd) {
-  printf("Setting up device...\n");
-
-  const sx1276::ChannelConfig kHardcodedChannelConfig {
-    .freq = 0xe4c000,
-    .bw = sx1276::Bandwidth::k125kHz,
-    .cr = sx1276::CodingRate::k4_7,
-    .sf = sx1276::SpreadingFactor::kSF9,
-  };
-
-  sx1276::init_lora(fd, kHardcodedChannelConfig);
-  printf("Setup complete!\n");
-}
-
-void transmit_iota_cmd(int fd, int ms, int val) {
-  printf("Transmitting...\n");
-  using RegAddr = sx1276::RegAddr;
-
-  spi_write_byte(fd, RegAddr::kOpMode, 0x89);
-  spi_write_byte(fd, RegAddr::kPreambleMsb, 0x00);
-  spi_write_byte(fd, RegAddr::kPreambleLsb, 0x08);
-  spi_write_byte(fd, RegAddr::kHopPeriod, 0x00);
-  spi_write_byte(fd, RegAddr::kPayloadLength, 0x04);
-  for (int i = 0; i < val; i++) {
-    spi_write_byte(fd, RegAddr::kIrqFlags, 0xff);  // clear interrupts
-    spi_write_byte(fd, RegAddr::kFifoTxBaseAddr, 0x00);
-    spi_write_byte(fd, RegAddr::kFifoAddrPtr, 0x00);
-
-    std::vector<uint8_t> data {'A',
-      static_cast<uint8_t>('0' + ((i / 100) % 10)),
-      static_cast<uint8_t>('0' + ((i / 10) % 10)),
-      static_cast<uint8_t>('0' + (i % 10)),
-      '\0'};
-    spi_write_burst(fd, 0x00, data.data(), 4);
-    spi_write_byte(fd, 0x01, 0x8b);
-    printf("Transmitted message #%d: %s\n", i, (char*)(data.data()));
-    usleep(ms * 1000);
-    //sleep(2);
-    spi_write_byte(fd, 0x01, 0x89);
-    usleep(1000);
-    //usleep(ms * 1000);
-  }
-}
-
-void transmit_cmd(int fd, int ms, const uint8_t* msg, int len) {
-  assert(len > 0);
-  assert(len <= kFifoMaxCapacity);
-
-  printf("Transmitting (len %d)...\n", len);
-  sx1276::lora_transmit(fd, ms, msg, len);
-  printf("Transmitted message %.*s\n", len, msg);
-}
-
 void diff_cmd(int fd) {
   printf("Recording SPI values... ");
   std::array<uint8_t, kMaxSpiAddress + 1> diff_buff{0};
@@ -201,51 +148,6 @@ int main() {
           continue;
         }
         burst_read_from_spi_cmd(fd, addr, val);
-      } else if (match_n("transmit-iota ", 15)) {
-        int ms {0};
-        sscanf(input_buffer + 15, "%d %d", &ms, &val);  // ms, number of transmissions
-        if (ms < 1) {
-          printf("Inter-message delay %d must be at least 1ms\n", ms);
-          continue;
-        }
-        if (val < 1) {
-          printf("Transmission sequence %d must be at least 1\n", val);
-          continue;
-        }
-        transmit_iota_cmd(fd, ms, val);
-      } else if (match_n("transmit ", 9)) {
-        const int kPrefixLen = 9 + 1;  // for the %
-        int ms {-1};
-        int consumed {0};
-        if (sscanf(input_buffer + kPrefixLen, "%d ", &ms)) {
-          if (ms < 1) {
-            printf("Inter-message delay %d must be at least 1ms\n", ms);
-            continue;
-          }
-          consumed = strcspn(input_buffer + kPrefixLen, " \t") + 1;
-        }
-        int msg_base_idx = kPrefixLen + consumed;
-        int msg_len = strcspn(input_buffer, "\0") - msg_base_idx;
-        if (ms == -1) {
-          // User did not provide a ToA, best-guess at one ourselves
-          // With current SF/CR/BW/etc. the minimum is around 97 for 1-3 chars;
-          // 4-8 needs 125 to be solid. Conservatively set the base @ 150 + the
-          // slope at 7/byte.
-          ms = 150 + (7 * msg_len);
-        }
-        printf("debug msg info: consumed %d strcspn %d len %d\n", consumed, msg_len + msg_base_idx, msg_len);
-        if (msg_len < 1) {
-          printf("Cannot send empty message\n");
-          continue;
-        }
-        if (msg_len > kFifoMaxCapacity) {
-          printf("Message length %d is greater than the maximum %d!\n", msg_len, kFifoMaxCapacity);
-          continue;
-        }
-        const uint8_t* msg = reinterpret_cast<uint8_t*>(input_buffer + msg_base_idx);
-        transmit_cmd(fd, ms, msg, msg_len);
-      } else if (match_n("init-transmit", 13)) {
-        hardcoded_init_for_transmit_cmd(fd);
       } else {
         printf("error: unknown command '%s'\n", input_buffer+1);
       }
